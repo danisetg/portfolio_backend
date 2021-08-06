974f5eb82316246a00b34d42e9916bb8c611c278
danisetg <danisetg@gmail.com>
2021-08-06 16:30:35 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:4200'
+        origins 'http://http://142.93.240.70/'
 
         resource '*',
         headers: :any,
